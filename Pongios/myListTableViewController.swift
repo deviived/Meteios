608e2f184cb9c5655a58bb36c225a231569d8047
PACHECO Luc <pachecoluc@mbp-de-admin-8.edu.sir>
2019-01-17 16:54:24 +0100

    blabla

--- Pongios/myListTableViewController.swift
+++ Pongios/myListTableViewController.swift
@@ -12,7 +12,6 @@ class myListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -24,23 +23,28 @@ class myListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as? Example1Cell
+            
+        else{
+            fatalError("ERROR SYSTEM!!!!")
+        }
+        
+        cell.textLabel?.text = "test"
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -90,5 +94,6 @@ class myListTableViewController: UITableViewController {
 }
 
 class Example1Cell: UITableViewCell {
-    @IBOutlet weak var title: UITextView!
+    
+    @IBOutlet weak var label: Example1Cell!
 }
